c3c356760acc30dba1902e5a62f012c36b8f6058
Your Name <you@example.com>
2024-09-23 19:13:57 +0500

    final

---
+++ main.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+const circle = document.getElementById('circle');
+const counter = document.getElementById('counter');
+const surprise = document.getElementById('surprise');
+
+circle.addEventListener('click', () => {
+    count++;
+    counter.textContent = count;
+
+    if (count === 10) {
+        surprise.classList.remove('hidden');
+    }
+});
